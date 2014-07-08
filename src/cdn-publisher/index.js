54f88372a2411997d0bcf4d45427667991b1bf7f
Ken Perkins <ken.perkins@rackspace.com>
2014-07-08 13:02:18 -0700

    Adding support for node-walk in lieu of home grown code

--- src/cdn-publisher/index.js
+++ src/cdn-publisher/index.js
@@ -3,44 +3,65 @@ var fs = require('fs'),
 	pkgcloud = require('pkgcloud'),
 	async = require('async'),
 	colors = require('colors'),
+  walk = require('walk'),
 	config = require(process.argv[2]);
 
 var client = pkgcloud.providers.rackspace.storage.createClient(config.pkgcloud);
 
+var walker = walk.walk(config.srcDirectory, {
+  followLinks: false
+});
+
 var files = [];
 
-function handleFile(path) {
-	if (fs.lstatSync(path).isDirectory()) {
-		fs.readdirSync(path).forEach(function(item) {
-			handleFile(path + '/' + item);
-		});
-	}
-	else {
-		files.push(path);
-	}
-}
-handleFile(config.srcDirectory);
-
-async.forEachLimit(files, 2, function(file, next) {
-	var remoteName = path.dirname(file.substring(1)) + '/' + path.basename(file.substring(1));
-
-	console.log(
-		'Uploading File ['.grey + file.green + '] to ['.grey + remoteName.yellow + ']'.grey
-	);
-	var stream = fs.createReadStream(file);
-
-	var options = {
-		container: config.destContainer,
-		remote: remoteName,
-		headers: {
-			'Access-Control-Allow-Origin': '*'
-		}
-	};
-
-	stream.pipe(dest);
-}, function(err) {
-	if (err) {
-		console.error(err);
-		process.exit(1);
-	}
+walker.on('file', function(root, fileStats, next) {
+  files.push(root + '/' + fileStats.name);
+  next();
 });
+
+walker.on('end', function() {
+  uploadFiles();
+});
+
+function uploadFiles() {
+  async.forEachLimit(files, 2, function (file, next) {
+    var localPath = path.relative(config.srcDirectory, file);
+    var remoteName = path.dirname(localPath) + '/' + path.basename(file.substring(1));
+
+    // first remove any path artifacts of the local directory
+    if (remoteName.indexOf('.') === 0) {
+      remoteName = remoteName.substring(1);
+    }
+
+    // then ensure the key includes a preceding slash
+    if (remoteName.substring(0,1) !== '/') {
+      remoteName = '/' + remoteName;
+    }
+
+    console.log(
+        'Uploading File ['.grey + file.green + '] to ['.grey + remoteName.yellow + ']'.grey
+    );
+
+    var stream = fs.createReadStream(file);
+
+    var options = {
+      container: config.destContainer,
+      remote: remoteName,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    };
+
+    var dest = client.upload(options, next);
+
+    stream.pipe(dest);
+
+    next();
+
+  }, function (err) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
+}
