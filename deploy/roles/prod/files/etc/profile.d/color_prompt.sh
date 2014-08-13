50477ae1c3972e84e904926326ded2df4470a44c
Shaunak Kashyap <ycombinator@gmail.com>
2014-08-13 16:27:02 -0700

    Using /root/.bashrc instead of /etc/profile.d/color_prompt.sh

--- deploy/roles/prod/files/etc/profile.d/color_prompt.sh
+++
@@ -1,6 +0,0 @@
-if [ "$color_prompt" = yes ]; then
-    PS1='\[\033[1;37m\]\[\033[41m\]${debian_chroot:+($debian_chroot)}\u@\h:\w\$\[\033[00m\] '
-else
-    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
-fi
-unset color_prompt force_color_prompt
